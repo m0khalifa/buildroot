a9d3cf3682c5f32f22c2bcf8736282c42e01dd95
Sanniti <sanniti.p@nyu.edu>
2021-01-25 17:23:54 -0500

    update 'pi3-miniuart-bt' to 'miniuart-bt'

--- board/opentrons/ot2/post-image.sh
+++ board/opentrons/ot2/post-image.sh
@@ -12,13 +12,13 @@ sed -i -e 's/console=ttyAMA0,115200//' "${BINARIES_DIR}/rpi-firmware/cmdline.txt
 for arg in "$@"
 do
 	case "${arg}" in
-		--add-pi3-miniuart-bt-overlay)
+		--add-miniuart-bt-overlay)
 		if ! grep -qE '^dtoverlay=' "${BINARIES_DIR}/rpi-firmware/config.txt"; then
-			echo "Adding 'dtoverlay=pi3-miniuart-bt' to config.txt (fixes ttyAMA0 serial console)."
+			echo "Adding 'dtoverlay=miniuart-bt' to config.txt (fixes ttyAMA0 serial console)."
 			cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
 
 # fixes rpi3 ttyAMA0 serial console
-dtoverlay=pi3-miniuart-bt
+dtoverlay=miniuart-bt
 __EOF__
 		fi
 		;;
